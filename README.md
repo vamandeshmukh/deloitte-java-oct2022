1fecec913c3b0594cd6671e73eddb5265066c0f3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-03 10:22:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# deloitte-java-oct2022
+ Core java demo project for Deloitte trainees Oct 2022
---
+++ src/deloitte/demo/Hello.java
@@ -0,0 +1,11 @@
+package deloitte.demo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+
+	}
+
+}
