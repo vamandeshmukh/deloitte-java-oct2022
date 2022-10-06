82ff957e66efdfc513294c4effd62b1c499a5e31
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-06 12:29:09 +0530

    day03

--- src/deloitte/demo/day03/MethodDemo.java
+++ src/deloitte/demo/day03/MethodDemo.java
@@ -5,16 +5,17 @@ public class MethodDemo {
 	public static void main(String[] args) {
 
 		MethodDemo.hello();
-		MethodDemo.hello();
-		MethodDemo.hello();
-		MethodDemo.hello();
-		MethodDemo.hello();
+		MethodDemo.hi("Sonu");
 
 	}
 
 	static void hello() {
 		// lot of code
 		System.out.println("Hello world!");
+	}
+
+	static void hi(String userName) {
+		System.out.println("Hi " + userName + "!");
 	}
 
 }
