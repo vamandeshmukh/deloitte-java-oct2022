53107d5649e5b1e165bd9c1f08ebb5e3508a65c1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-06 11:09:20 +0530

    day03

---
+++ src/deloitte/demo/day03/InputDemo.java
@@ -0,0 +1,21 @@
+package deloitte.demo.day03;
+
+public class InputDemo {
+
+	public static void main(String[] args) {
+
+		String input1 = "10";
+		String input2 = "20";
+
+		int num1 = Integer.parseInt(input1);
+		int num2 = Integer.parseInt(input2);
+
+		int num3 = num1 + num2;
+
+		String output = Integer.toString(num3);
+
+		System.out.println(output);
+
+	}
+
+}
---
+++ src/deloitte/demo/day03/WrapperDemo.java
@@ -0,0 +1,25 @@
+package deloitte.demo.day03;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+
+		// byte - Byte 
+		// short - Short 
+		// int - Integer 
+		// long - Long 
+		// float - Float 
+		// double - Double 
+		// char - Character 
+		// boolean - Boolean 
+
+//		int pin = 500001;
+		Integer pin = 500001;
+		String firstName = "Vaman";
+
+//		pin.
+//		firstName.
+
+	}
+
+}
