76d8ff4646bb658748481234e3f83c3b166134f5
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-06 12:41:02 +0530

    day03

--- src/deloitte/demo/day03/MethodDemo.java
+++ src/deloitte/demo/day03/MethodDemo.java
@@ -4,18 +4,40 @@ public class MethodDemo {
 
 	public static void main(String[] args) {
 
-		MethodDemo.hello();
-		MethodDemo.hi("Sonu");
+//		MethodDemo.hi("Sonu");
+//		MethodDemo.hi2("Sonu", "Sood");
+
+		MethodDemo.printTen();
+		MethodDemo.getTenAndPrint(10);
+		int i = MethodDemo.returnTen();
+		System.out.println(i);
+		int j = MethodDemo.getTenAndReturn(10);
+		System.out.println(j);
 
 	}
 
-	static void hello() {
-		// lot of code
-		System.out.println("Hello world!");
+//	static void hi(String userName) { // with arguments, without return value
+//		System.out.println("Hi " + userName + "!");
+//	}
+//
+//	static void hi2(String firstName, String lastName) { // with arguments, without return value
+//		System.out.println("Hi " + firstName + " " + lastName + "!");
+//	}
+
+	static void printTen() { // No Arguments, No Return value - NANR
+		System.out.println(10);
 	}
 
-	static void hi(String userName) {
-		System.out.println("Hi " + userName + "!");
+	static void getTenAndPrint(int num) { // With Arguments, No return value - WANR
+		System.out.println(num);
+		;
 	}
 
+	static int returnTen() { // No Arguments, With Return value - NAWR
+		return 10;
+	}
+
+	static int getTenAndReturn(int num) { // With Arguments, with return value - NWWR
+		return num;
+	}
 }
