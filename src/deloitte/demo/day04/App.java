2f80423ea9df7122ac67fdc67ee5b4584851f277
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-07 10:11:00 +0530

    m

---
+++ src/deloitte/demo/day04/App.java
@@ -0,0 +1,37 @@
+package deloitte.demo.day04;
+
+public class App {
+
+	static String today;
+
+	public static void main(String[] args) {
+
+		System.out.println(App.today);
+
+	}
+
+}
+
+//package deloitte.demo.day04;
+//
+//public class App {
+//	
+//	static String today;
+//
+//
+//	public static void main(String[] args) {
+//		
+//		
+//		System.out.println(App.today);
+//		
+//		App.today = "Friday";
+//		
+//		System.out.println(App.today);
+//
+//
+//		App.today = "Boilday"; // X 
+//		
+//		System.out.println(App.today);
+//	}
+//
+//}
