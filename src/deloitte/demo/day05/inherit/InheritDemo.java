b158a13895449e19078243b49be448c0e0397a2b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-10 12:01:03 +0530

    OOPs

--- src/deloitte/demo/day05/inherit/InheritDemo.java
+++ src/deloitte/demo/day05/inherit/InheritDemo.java
@@ -1,19 +1,38 @@
 package deloitte.demo.day05.inherit;
 
+class A {
+
+}
+
+class B {
+
+}
+
+class C extends A {
+
+}
+
 public class InheritDemo {
 
 	public static void main(String[] args) {
 
-		System.out.println("basic phone");
+		System.out.println("basic phone:");
 		Phone phone1 = new Phone();
 		phone1.call();
 		phone1.sms();
 
-		System.out.println("Feature phone");
+		System.out.println("Feature phone:");
 		FeaturePhone phone2 = new FeaturePhone();
 		phone2.call();
 		phone2.sms();
 		phone2.music();
+
+		System.out.println("Smart phone:");
+		SmartPhone phone3 = new SmartPhone();
+		phone3.call();
+		phone3.sms();
+		phone3.music();
+		phone3.camera();
 
 	}
 
---
+++ src/deloitte/demo/day05/inherit/SmartPhone.java
@@ -0,0 +1,9 @@
+package deloitte.demo.day05.inherit;
+
+public class SmartPhone extends FeaturePhone {
+
+	public void camera() {
+		System.out.println("Clicking pics...");
+	}
+
+}
