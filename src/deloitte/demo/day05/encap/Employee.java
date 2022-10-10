4c786243a59b2b307c4cec2265cbfbec4f9cc396
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-10 11:11:44 +0530

    OOPs

--- src/deloitte/demo/day05/encap/Employee.java
+++ src/deloitte/demo/day05/encap/Employee.java
@@ -1,10 +1,39 @@
 package deloitte.demo.day05.encap;
 
+// POJO class 
+
 public class Employee {
 
 	private int eid;
 	private String firstName;
 	private double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int eid, String firstName, double salary) {
+		super();
+		this.eid = eid;
+		this.firstName = firstName;
+		this.salary = salary;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 	public double getSalary() {
 		return salary;
