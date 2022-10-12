2040b0a65fa659aab57197a1603a6cc3e80e904b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-12 10:51:57 +0530

    SQL

--- deloitte.sql
+++ deloitte.sql
@@ -35,5 +35,14 @@ UPDATE emp_table SET salary = 86000 WHERE eid = 103;
 
 SELECT * FROM emp_table;
 
+DELETE FROM emp_table WHERE eid = 103;
 
+INSERT INTO emp_table (eid, first_name, salary) VALUES (105, 'Gonu', 99000);
 
+CREATE TABLE emp2 (eid INT PRIMARY KEY, first_name VARCHAR(10), salary DECIMAL);
+
+INSERT INTO emp2 (eid, first_name, salary) VALUES (101, 'Sonu', 90000);
+INSERT INTO emp2 (eid, first_name, salary) VALUES (102, 'Monu', 95000);
+INSERT INTO emp2 (eid, first_name, salary) VALUES (102, 'Monu', 95000);
+
+SELECT * FROM emp2;
