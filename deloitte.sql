5b2baa24ef231aafabdbcc2c0845a4db696f91a1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-12 10:17:20 +0530

    SQL

--- deloitte.sql
+++ deloitte.sql
@@ -1,5 +1,25 @@
 
-
-select * from actor;
-
 select current_date();
+
+-- SQL - Structured Query Language  -- 
+-- MySQL, SQL Server, Oracle, DB2, Postgre
+
+-- CRUD
+-- Create -  INSERT - DML
+-- Update - UPDATE - DML
+-- Delete - DELETE - DML
+-- Retrieve -  SELECT - DRL / DQL 
+
+-- CREATE - DDL 
+-- DROP - DDL 
+-- ALTER - DDL 
+
+
+CREATE TABLE emp_table (eid INT, first_name VARCHAR(10), salary DECIMAL);
+
+SELECT * FROM emp_table;
+
+INSERT INTO emp_table (eid, first_name, salary) VALUES (101, 'Sonu', 90000);
+
+
+
