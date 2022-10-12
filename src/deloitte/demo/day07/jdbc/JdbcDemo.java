a00a896b243e333129085292ec2d2ecbcdc45072
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-12 14:21:21 +0530

    JDBC

--- src/deloitte/demo/day07/jdbc/JdbcDemo.java
+++ src/deloitte/demo/day07/jdbc/JdbcDemo.java
@@ -1,13 +1,17 @@
 package deloitte.demo.day07.jdbc;
 
+// make a connection to database 
+// execute a query 
+// process the result-set 
+
 public class JdbcDemo {
 
 	public static void main(String[] args) {
-		
-		// make a connection to database 
-		// execute a query 
-		// process the result-set 
-		
+
+		String url;
+		String user;
+		String password;
+		String sql;
 
 	}
 
