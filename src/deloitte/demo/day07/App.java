d53520762721287d0df9f75e2e436287c5b61ef1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-12 14:17:55 +0530

    JDBC

---
+++ src/deloitte/demo/day07/App.java
@@ -0,0 +1,10 @@
+package deloitte.demo.day07;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/deloitte/demo/day07/jdbc/JdbcDemo.java
@@ -0,0 +1,14 @@
+package deloitte.demo.day07.jdbc;
+
+public class JdbcDemo {
+
+	public static void main(String[] args) {
+		
+		// make a connection to database 
+		// execute a query 
+		// process the result-set 
+		
+
+	}
+
+}
