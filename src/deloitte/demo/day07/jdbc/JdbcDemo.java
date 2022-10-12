841c7847ca6192232c1188e8a907185bad9a9389
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-12 14:35:04 +0530

    JDBC

--- src/deloitte/demo/day07/jdbc/JdbcDemo.java
+++ src/deloitte/demo/day07/jdbc/JdbcDemo.java
@@ -1,5 +1,11 @@
 package deloitte.demo.day07.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 // make a connection to database 
 // execute a query 
 // process the result-set 
@@ -7,12 +13,32 @@ package deloitte.demo.day07.jdbc;
 public class JdbcDemo {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Start");
 
-		String url;
-		String user;
-		String password;
-		String sql;
+		String url = "jdbc:mysql://localhost:3306/deloitte_oct22";
+		String user = "root";
+		String password = "root";
+		String sql = "SELECT * FROM emp WHERE eid = 101";
 
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println(con);
+			st = con.createStatement();
+			rs = st.executeQuery(sql);
+			System.out.println(rs.getInt(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getDouble(3));
+			System.out.println(rs.getInt(4));
+
+		} catch (SQLException e) {
+			System.out.println("Something is wrong!");
+		}
+		System.out.println("End");
 	}
 
 }
